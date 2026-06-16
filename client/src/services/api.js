854566e105ce8e7817963ccb0b3e89d3bd97c80b
Jayprakash Thakur <mantuthakur2943@gmail.com>
2026-06-16 23:13:26 +0530

    Frontend Design Deployed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2401/api"
+  // baseURL: "http://localhost:2401/api"
+  baseURL: "https://personalworktracker.vercel.app/"
 });
 
 export default api;
