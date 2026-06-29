099496ace54b422b5de27a123e363106875260bf
Jayprakash Thakur <mantuthakur2943@gmail.com>
2026-06-29 22:17:44 +0530

    Added Taoster properly and fix Css Issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,15 +7,21 @@ import "./styles/dashboard.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <ToastContainer
       position="top-right"
-      autoClose={3000}
+      autoClose={2500}
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick
+      pauseOnHover
+      draggable
       theme="colored"
+      className="custom-toast-container"
+      toastClassName="custom-toast"
+      bodyClassName="custom-toast-body"
     />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
